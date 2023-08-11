167f98f616ee860c7ff336da8949607e10b2a4cd
Varad Rajopadhye <varadmanraj@gmail.comm>
2023-08-11 09:48:49 +0530

    Added Database password reset

--- hooked.sql
+++ hooked.sql
@@ -1,4 +1,5 @@
 -- Create the database if it doesn't exist
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password';
 CREATE DATABASE IF NOT EXISTS hooked;
 
 -- Use the created database
